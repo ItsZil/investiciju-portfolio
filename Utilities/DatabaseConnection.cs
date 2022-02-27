4634c68846c4f800edbda35beb73ee3d0f9825e3
Žilvinas K <zilvinas11711@gmail.com>
2022-02-27 19:22:07 +0200

    Method to open connection

--- Utilities/DatabaseConnection.cs
+++ Utilities/DatabaseConnection.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
 
 namespace investiciju_portfolio.Utilities
 {
-    internal class DatabaseConnection
+    public class DatabaseConnection
     {
+        private string connectionString = "server=localhost;user=investiciju_portfolio;password=ipprojektas#;database=investiciju_portfolio";
+        MySqlConnection connection;
+
+        /// <summary>
+        /// Opens a connection to the database server.
+        /// </summary>
+        /// <returns>A boolean indicating if connected successfully.</returns>
+        public bool OpenConnection()
+        {
+            if (connectionString.Length == 0)
+                return false; // Maybe throw an exception
+
+            connection = new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to open connection with database: " + ex.Message);
+                return false;
+            }
+            return true;
+        }
     }
 }
