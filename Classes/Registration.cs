f722ddc3d062b9d3ab32f67dfb33f74e58c98bde
Žilvinas K <22817405+ItsZil@users.noreply.github.com>
2022-04-05 21:09:58 +0300

    RegisterUser method

--- Classes/Registration.cs
+++ Classes/Registration.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MySqlConnector;
 using investiciju_portfolio.Utilities;
 
 namespace investiciju_portfolio.Classes
@@ -10,8 +7,37 @@ namespace investiciju_portfolio.Classes
     /// <summary>
     /// Class to handle registration of new users.
     /// </summary>
-    internal class Registration
+    internal static class Registration
     {
-        DatabaseConnection dbConnection = new DatabaseConnection();
+        /// <summary>
+        /// Registers a new user.
+        /// </summary>
+        /// <param name="username">Username</param>
+        /// <param name="name">First name</param>
+        /// <param name="surname">Surname</param>
+        /// <param name="password">Plain text password</param>
+        /// <returns></returns>
+        public static bool RegisterUser(string username, string name, string surname, string password)
+        {
+            try
+            {
+                Password userPassword = PasswordHandler.CreateHash(password);
+                DatabaseConnection dbConnection = new DatabaseConnection();
+
+                MySqlCommand command = new MySqlCommand(String.Format("INSERT INTO users (username, password, salt, first_name, last_name) VALUES (@username, @password, @salt, @first_name, @surname)"));
+                command.Parameters.AddWithValue("@username", username);
+                command.Parameters.AddWithValue("@password", userPassword.Hash);
+                command.Parameters.AddWithValue("@salt", userPassword.Salt);
+                command.Parameters.AddWithValue("@first_name", name);
+                command.Parameters.AddWithValue("@surname", surname);
+
+                dbConnection.ExecuteCommand(command);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
     }
 }
