529bbf1f44c9aa68dcc7048c03e54aaac944a377
Žilvinas K <22817405+ItsZil@users.noreply.github.com>
2022-04-05 20:50:26 +0300

    Implement password hashing method

--- Utilities/PasswordHandler.cs
+++ Utilities/PasswordHandler.cs
@@ -12,6 +12,10 @@ namespace investiciju_portfolio.Utilities
     /// </summary>
     static internal class PasswordHandler
     {
+        public const int SALT_SIZE = 24; // Size in bytes
+        public const int HASH_SIZE = 24; // Size in bytes
+        public const int ITERATIONS = 100000; // Number of PBKDF2 iterations
+
         /// <summary>
         /// Checks if a password meets the requirement of containing 1 number and 1 capital letter.
         /// </summary>
@@ -19,9 +23,30 @@ namespace investiciju_portfolio.Utilities
         /// <returns>true if meets requirements, false otherwise.</returns>
         public static bool PasswordMeetsRequirements(string password)
         {
-            return (password.Any(c => char.IsDigit(c))) &&
+            return password.Length >= 8 &&
+                password.Any(c => char.IsDigit(c)) &&
                 password.Any(c => char.IsUpper(c)) &&
                 password.Any(c => char.IsLower(c));
+        }
+
+        /// <summary>
+        /// Hashes a password string using the PBKDF2 algorithm.
+        /// </summary>
+        /// <param name="password">Plain text password string.</param>
+        /// <returns>A string of the hashed password.</returns>
+        public static string CreateHash(string password)
+        {
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+
+            // Generate unique random salt
+            byte[] salt = new byte[SALT_SIZE];
+            provider.GetBytes(salt);
+
+            // Generate password hash
+            Rfc2898DeriveBytes hash = new Rfc2898DeriveBytes(password, salt, ITERATIONS);
+            string hashedPassword = hash.GetBytes(HASH_SIZE).ToString();
+
+            return hashedPassword;
         }
     }
 }
