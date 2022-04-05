48ecd972a18f93c2f9ebd42d45eaae0b3e793e2b
Žilvinas K <22817405+ItsZil@users.noreply.github.com>
2022-04-05 20:11:35 +0300

    Create registration classes

---
+++ Classes/Registration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using investiciju_portfolio.Utilities;
+
+namespace investiciju_portfolio.Classes
+{
+    /// <summary>
+    /// Class to handle registration of new users.
+    /// </summary>
+    internal class Registration
+    {
+        DatabaseConnection dbConnection = new DatabaseConnection();
+    }
+}
---
+++ Utilities/PasswordHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace investiciju_portfolio.Utilities
+{
+    /// <summary>
+    /// Static class containing methods to assist in hashing and unhashing passwords.
+    /// </summary>
+    static internal class PasswordHandler
+    {
+    }
+}
