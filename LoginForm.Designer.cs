7c23682de72b31d213afde6b8dcc5368e0ae9b22
Ostinas <ostinasmaciulis@gmail.com>
2022-04-08 00:25:20 +0300

    Implemented new TextBox in Login and Registration forms

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -28,30 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.LoginForm_HeaderLabel = new System.Windows.Forms.Label();
             this.LoginForm_LoginButton = new System.Windows.Forms.Button();
             this.LoginForm_RegisterButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.LoginForm_TurnOffButton = new System.Windows.Forms.Button();
             this.Logo_TextBox = new System.Windows.Forms.PictureBox();
-            this.LoginForm_UsernameTextBox = new System.Windows.Forms.TextBox();
-            this.LoginForm_PasswordTextBox = new System.Windows.Forms.TextBox();
-            this.WatermarkTextBox1 = new investiciju_portfolio.Classes.WatermarkTextBox();
+            this.LoginForm_PasswordTextBox = new investiciju_portfolio.Classes.WatermarkTextBox();
+            this.LoginForm_UsernameTextBox = new investiciju_portfolio.Classes.WatermarkTextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Logo_TextBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // LoginForm_HeaderLabel
             // 
-            this.label1.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(56)))));
-            this.label1.Location = new System.Drawing.Point(500, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(392, 77);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Investicijų portfolio";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.LoginForm_HeaderLabel.Font = new System.Drawing.Font("Verdana", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginForm_HeaderLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(56)))));
+            this.LoginForm_HeaderLabel.Location = new System.Drawing.Point(500, 0);
+            this.LoginForm_HeaderLabel.Name = "LoginForm_HeaderLabel";
+            this.LoginForm_HeaderLabel.Size = new System.Drawing.Size(392, 77);
+            this.LoginForm_HeaderLabel.TabIndex = 1;
+            this.LoginForm_HeaderLabel.Text = "Investicijų portfolio";
+            this.LoginForm_HeaderLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // LoginForm_LoginButton
             // 
@@ -66,7 +64,7 @@
             this.LoginForm_LoginButton.TabIndex = 2;
             this.LoginForm_LoginButton.Text = "Log in";
             this.LoginForm_LoginButton.UseVisualStyleBackColor = false;
-            this.LoginForm_LoginButton.Click += new System.EventHandler(this.button1_Click);
+            this.LoginForm_LoginButton.Click += new System.EventHandler(this.LoginForm_LoginButton_Click);
             // 
             // LoginForm_RegisterButton
             // 
@@ -81,14 +79,14 @@
             this.LoginForm_RegisterButton.TabIndex = 3;
             this.LoginForm_RegisterButton.Text = "Register";
             this.LoginForm_RegisterButton.UseVisualStyleBackColor = false;
-            this.LoginForm_RegisterButton.Click += new System.EventHandler(this.button2_Click);
+            this.LoginForm_RegisterButton.Click += new System.EventHandler(this.LoginForm_RegisterButton_Click);
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(101)))), ((int)(((byte)(67)))));
             this.panel1.Controls.Add(this.LoginForm_TurnOffButton);
             this.panel1.Controls.Add(this.Logo_TextBox);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.LoginForm_HeaderLabel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
@@ -118,48 +116,34 @@
             this.Logo_TextBox.TabIndex = 2;
             this.Logo_TextBox.TabStop = false;
             // 
-            // LoginForm_UsernameTextBox
-            // 
-            this.LoginForm_UsernameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(101)))), ((int)(((byte)(67)))));
-            this.LoginForm_UsernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.LoginForm_UsernameTextBox.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LoginForm_UsernameTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(56)))));
-            this.LoginForm_UsernameTextBox.Location = new System.Drawing.Point(507, 183);
-            this.LoginForm_UsernameTextBox.MaxLength = 64;
-            this.LoginForm_UsernameTextBox.Name = "LoginForm_UsernameTextBox";
-            this.LoginForm_UsernameTextBox.Size = new System.Drawing.Size(385, 25);
-            this.LoginForm_UsernameTextBox.TabIndex = 12;
-            this.LoginForm_UsernameTextBox.Text = "Username";
-            this.LoginForm_UsernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.LoginForm_UsernameTextBox.TextChanged += new System.EventHandler(this.LoginForm_UsernameTextBox_TextChanged);
-            // 
             // LoginForm_PasswordTextBox
             // 
             this.LoginForm_PasswordTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(101)))), ((int)(((byte)(67)))));
             this.LoginForm_PasswordTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.LoginForm_PasswordTextBox.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.LoginForm_PasswordTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(56)))));
-            this.LoginForm_PasswordTextBox.Location = new System.Drawing.Point(507, 241);
+            this.LoginForm_PasswordTextBox.Hint = "Password";
+            this.LoginForm_PasswordTextBox.Location = new System.Drawing.Point(507, 229);
             this.LoginForm_PasswordTextBox.MaxLength = 256;
             this.LoginForm_PasswordTextBox.Name = "LoginForm_PasswordTextBox";
             this.LoginForm_PasswordTextBox.PasswordChar = '*';
             this.LoginForm_PasswordTextBox.Size = new System.Drawing.Size(385, 25);
             this.LoginForm_PasswordTextBox.TabIndex = 13;
-            this.LoginForm_PasswordTextBox.Text = "Password";
             this.LoginForm_PasswordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // WatermarkTextBox1
+            // LoginForm_UsernameTextBox
             // 
-            this.WatermarkTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(101)))), ((int)(((byte)(67)))));
-            this.WatermarkTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.WatermarkTextBox1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WatermarkTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(56)))));
-            this.WatermarkTextBox1.Hint = "Username";
-            this.WatermarkTextBox1.Location = new System.Drawing.Point(507, 139);
-            this.WatermarkTextBox1.Name = "WatermarkTextBox1";
-            this.WatermarkTextBox1.Size = new System.Drawing.Size(385, 25);
-            this.WatermarkTextBox1.TabIndex = 18;
-            this.WatermarkTextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.LoginForm_UsernameTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(101)))), ((int)(((byte)(67)))));
+            this.LoginForm_UsernameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LoginForm_UsernameTextBox.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginForm_UsernameTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(56)))));
+            this.LoginForm_UsernameTextBox.Hint = "Username";
+            this.LoginForm_UsernameTextBox.Location = new System.Drawing.Point(507, 174);
+            this.LoginForm_UsernameTextBox.MaxLength = 64;
+            this.LoginForm_UsernameTextBox.Name = "LoginForm_UsernameTextBox";
+            this.LoginForm_UsernameTextBox.Size = new System.Drawing.Size(385, 25);
+            this.LoginForm_UsernameTextBox.TabIndex = 12;
+            this.LoginForm_UsernameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // LoginForm
             // 
@@ -167,7 +151,6 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(54)))), ((int)(((byte)(56)))));
             this.ClientSize = new System.Drawing.Size(1382, 593);
-            this.Controls.Add(this.WatermarkTextBox1);
             this.Controls.Add(this.LoginForm_PasswordTextBox);
             this.Controls.Add(this.LoginForm_UsernameTextBox);
             this.Controls.Add(this.panel1);
@@ -188,15 +171,14 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LoginForm_HeaderLabel;
         private System.Windows.Forms.Button LoginForm_LoginButton;
         private System.Windows.Forms.Button LoginForm_RegisterButton;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.TextBox LoginForm_UsernameTextBox;
-        private System.Windows.Forms.TextBox LoginForm_PasswordTextBox;
         private System.Windows.Forms.PictureBox Logo_TextBox;
         private System.Windows.Forms.Button LoginForm_TurnOffButton;
-        private Classes.WatermarkTextBox WatermarkTextBox1;
+        private Classes.WatermarkTextBox LoginForm_UsernameTextBox;
+        private Classes.WatermarkTextBox LoginForm_PasswordTextBox;
     }
 }
 
