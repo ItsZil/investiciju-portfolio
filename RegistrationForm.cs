37d3b08c172ba0edf44ce0d816c281cf9afdda4c
TadisOn <76095383+TadisOn@users.noreply.github.com>
2022-04-11 22:53:35 +0300

    Field verification

--- RegistrationForm.cs
+++ RegistrationForm.cs
@@ -21,20 +21,28 @@ namespace investiciju_portfolio
 
             // IP-37 field verification: need to check if strings aren't empty, show errors
 
+            if(!Registration.IsUsernameUnique(usernameText) && !PasswordHandler.PasswordMeetsRequirements(passwordText))
+            {
+                MessageBox.Show("Username is already taken. \n" + "Password must contain 8 symbols, atleast 1 digit and 1 capital letter.");
+            }
+
             if (!Registration.IsUsernameUnique(usernameText))
-            { 
-                // error message
+            {
+                MessageBox.Show("Username is already taken.");
                 return;
             }
 
             if (!PasswordHandler.PasswordMeetsRequirements(passwordText))
             {
-                // password does not contain at least 8 chars, or 1 capital letter, or 1 digit
+                MessageBox.Show("Password must contain 8 symbols, atleast 1 digit and 1 capital letter.");
                 return;
             }
 
             bool registrationState = Registration.RegisterUser(usernameText, nameText, surnameText, passwordText);
-            // check if registration is successful, show message if not
+            if(registrationState)
+            {
+                MessageBox.Show("Registration was succesful.");
+            }
         }
 
         private void RegistrationForm_Load(object sender, EventArgs e)
