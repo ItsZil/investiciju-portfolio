4292569d04c3ae79197de773cd2ca204c7e87b27
Žilvinas K <22817405+ItsZil@users.noreply.github.com>
2022-05-11 19:12:23 +0300

    Load and save base currency setting

--- Forms/SettingsTab.cs
+++ Forms/SettingsTab.cs
@@ -22,7 +22,17 @@ namespace investiciju_portfolio
             //SettingsTab_UsernameDisplayLabel.Text = 
             //SettingsTab_NameDisplayLabel.Text = 
             //SettingsTab_SurnameDisplayLabel.Text = 
-            SettingsTab_CurrencySelectionBox.Text = SettingsTab_CurrencySelectionBox.Items[0].ToString();
+            SettingsTab_CurrencySelectionBox.Text = SettingsTab_CurrencySelectionBox.Items[Properties.Settings.Default.base_currency].ToString();
+        }
+
+        private void SettingsTab_SaveCurrencyButton_Click(object sender, EventArgs e)
+        {
+            int selection = SettingsTab_CurrencySelectionBox.SelectedIndex;
+            if (selection != -1)
+            {
+                Properties.Settings.Default.base_currency = selection;
+                Properties.Settings.Default.Save();
+            }
         }
     }
 }
