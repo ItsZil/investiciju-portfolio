40f60af7db07eab0abec0882bb8b495852f854e9
Žilvinas K <22817405+ItsZil@users.noreply.github.com>
2022-05-12 17:26:41 +0300

    Refactor Login.cs password verification

--- Classes/Login.cs
+++ Classes/Login.cs
@@ -10,43 +10,66 @@ namespace investiciju_portfolio.Classes
     internal static class Login
     {
         /// <summary>
-        /// Registers a new user.
+        /// Checks if the user's password is correct.
         /// </summary>
         /// <param name="username">Username</param>
-        /// <param name="password">Users password</param>
-        /// <returns></returns>
+        /// <param name="password">User password hash</param>
+        /// <returns>True if logged in successfully, false otherwise.</returns>
         public static bool CheckLogin(string username, string password)
         {
-            
-                MySqlConnection con;
-                MySqlCommand cmd;
-                MySqlDataReader dr;
-                con = new MySqlConnection("server=localhost;user=investiciju_portfolio;password=ipprojektas#;database=investiciju_portfolio");
-                cmd = new MySqlCommand();
-                con.Open();
-                cmd.Connection = con;
-                cmd.CommandText = "SELECT * FROM `users` where username='" + username + "'";
-                dr = cmd.ExecuteReader();
-                if (dr.Read())
+            bool passwordCorrect = VerifyPassword(username, password, true);
+            if (passwordCorrect)
+            {
+                return true;
+            }
+            else
+                System.Windows.Forms.MessageBox.Show("Invalid username and/or incorrect password.");
+            return false;
+        }
+
+        /// <summary>
+        /// Verifies if the password hash is correct.
+        /// </summary>
+        /// <param name="username">Username</param>
+        /// <param name="password">User password hash</param>
+        /// <param name="setSettings">Whether to set local user settings</param>
+        /// <returns>True if password is correct, false otherwise.</returns>
+        public static bool VerifyPassword(string username, string password, bool setSettings)
+        {
+            MySqlConnection con;
+            MySqlCommand cmd;
+            MySqlDataReader dr;
+            con = new MySqlConnection("server=localhost;user=investiciju_portfolio;password=ipprojektas#;database=investiciju_portfolio");
+            cmd = new MySqlCommand();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM `users` where username='" + username + "'";
+            dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                var passwordHasher = new PasswordHasher();
+                var isVerified = passwordHasher.VerifyHashedPassword(dr["password"].ToString(), password) != PasswordVerificationResult.Failed;
+                if (isVerified)
                 {
-                    var passwordHasher = new PasswordHasher();
-                    var isVerified = passwordHasher.VerifyHashedPassword(dr["password"].ToString(), password) != PasswordVerificationResult.Failed;
-                    if (isVerified)
+                    if (setSettings)
                     {
                         // Set settings before logging in.
                         SettingsHandler.SetSettings(dr["username"].ToString(), dr["first_name"].ToString(), dr["surname"].ToString(), (int)dr["id"]);
-                        return true;
                     }
-                    else
-                        System.Windows.Forms.MessageBox.Show("Invalid username and/or incorrect password.");
+                    con.Close();
+                    return true;
                 }
                 else
                 {
-                    System.Windows.Forms.MessageBox.Show("Invalid username and/or incorrect password.");
+                    con.Close();
+                    return false;
                 }
+            }
+            else
+            {
                 con.Close();
-
-            return false;
+                return false;
+            }
         }
     }
 }
