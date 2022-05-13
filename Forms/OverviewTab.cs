4a508f0c31a55f3795aad87568173787feb6f065
Žilvinas K <22817405+ItsZil@users.noreply.github.com>
2022-05-13 22:02:28 +0300

    Revert "Merge branch 'master' into IP-42-implement-create-edit-and-delete-instrument-buttons"
    This reverts commit 16bcb4d1a2508b6582f31b2c32069e2aa74c4359, reversing
    changes made to 96c75544da019c1d42ab2494688a762b26dc0b8c.

--- Forms/OverviewTab.cs
+++ Forms/OverviewTab.cs
@@ -12,13 +12,8 @@ namespace investiciju_portfolio
 {
     public partial class OverviewTab : UserControl
     {
-        //API:ESVY1L813KK0BO7V
         public OverviewTab()
         {
-            StockAPI conn = new StockAPI("ESVY1L813KK0BO7V");
-            double price = conn.getPrice("IBM",0);
-            double price2DaysAgo = conn.getPrice("IBM", 2);
-
             InitializeComponent();
         }
 
