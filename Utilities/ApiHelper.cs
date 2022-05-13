52c21d4fbe6df3cdbaa0b287224a771944389389
TadisOn <76095383+TadisOn@users.noreply.github.com>
2022-05-13 15:24:00 +0300

    Updated StockAPI #2
    -Added comments
    -Renamed class
    -Added getPrice() method

--- Utilities/ApiHelper.cs
+++ Utilities/ApiHelper.cs
@@ -10,17 +10,21 @@ using Microsoft.Data.Analysis;
 
 namespace investiciju_portfolio
 {
-    public class ApiHelper
+    public class StockAPI
     {
         //ESVY1L813KK0BO7V
        
         private string _apiKey;
 
-        public ApiHelper(string apiKey)
+        public StockAPI(string apiKey = "ESVY1L813KK0BO7V")
         {
             this._apiKey = apiKey;
         }
 
+        /// <summary>
+        /// Saves stock's data to csv file
+        /// </summary>
+        /// <param name="symbol"> stock's ticker </param>
         public void SaveCSVFromURL(string symbol)
         {
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://" + $@"www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={this._apiKey}&datatype=csv");
@@ -33,17 +37,24 @@ namespace investiciju_portfolio
 
         }
 
-        public double getPrice(string stock)
+
+        /// <summary>
+        /// Returns price from csv file
+        /// </summary>
+        /// <param name="stock"> stock's ticker </param>
+        /// <param name="rowsInd"> number of days passed from today (Assumption) </param>
+        /// <returns></returns>
+        public double getPrice(string stock, int rowsInd)
         {
             SaveCSVFromURL(stock);
 
 
             DataFrame df = DataFrame.LoadCsv("stockdata.csv");
 
-            DataFrameRow row = df.Rows[0];
+            DataFrameRow row = df.Rows[rowsInd];
 
-            string price = row[1].ToString();
-            return 2;
+            double price = Convert.ToDouble(row[1]);
+            return price;
         }
     }
 }
