272044924eee84707b3abb144fe7ac48dd64a1c0
TadisOn <76095383+TadisOn@users.noreply.github.com>
2022-05-13 14:11:16 +0300

    Implemented API
    Implemented API class with a method to save stocks data to csv by given symbol

---
+++ Utilities/ApiHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace investiciju_portfolio.Utilities
+{
+    public class ApiHelper
+    {
+        //ESVY1L813KK0BO7V
+       
+        private string _apiKey;
+
+        public ApiHelper(string apiKey)
+        {
+            this._apiKey = apiKey;
+        }
+
+        public void SaveCSVFromURL(string symbol)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://" + $@"www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={this._apiKey}&datatype=csv");
+            HttpWebResponse resp = (HttpWebResponse)req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            string results = sr.ReadToEnd();
+            sr.Close();
+            File.WriteAllText("stockdata.csv", results);
+
+        }
+    }
+}
